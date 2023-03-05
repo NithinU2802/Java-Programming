11483f41520ea41f14a43dde5e5cf645c14e063f
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-03-05 21:21:37 +0530

    Create readLine.java

---
+++ BufferedReader/readLine.java
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+Matrix multiplication......
+
+
+Input:
+12
+1
+
+Output:
+12 1
+
+
+Input:
+12
+110
+
+Output:
+12 110
+
+
+*******************************************************************************/
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException
+	{
+	BufferedReader x=new BufferedReader(new InputStreamReader(System.in));
+    String s=x.readLine(),a=x.readLine();
+    System.out.println(s+" "+a);
+	}
+}
