93c1bb495bcbe31151d379c569548c53e6c7f772
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-03-17 09:23:35 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class demo
+{
+    public static void main(String arg[]){
+    Scanner x=new Scanner(System.in);
+    String a=x.nextLine();
+    System.out.print("Welcome You in Programming "+a);
+ }
+}
