e4ae54234adc32af221a38618e69cd099aed252b
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-09-17 22:55:39 +0530

    Create WithoutSemiColon.java

---
+++ Tricky_Questions/WithoutSemiColon.java
@@ -0,0 +1,12 @@
+/******************************************************************************
+
+    Print String withour Semicolon
+
+*******************************************************************************/
+
+public class Main {
+    public static void main(String[] args) {
+        if (System.out.printf("%s\n",System.out.printf("Hello world\n"))==null) {
+        }
+    }
+}
