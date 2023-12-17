0f420b86372cc6d2a95cb803f710dec0e797cc72
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-12-17 20:56:01 +0530

    Create Compare.java

---
+++ StringMethods/Compare.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main 
+{ 
+	public static void main(String[] args) 
+	{ 
+		String s1 = "lafzximxh"; 
+		String s2 = "ee"; 
+
+		System.out.println(" Comparing strings with compareTo:"); 
+		System.out.println(s1.compareTo(s2)); 
+		System.out.println(s2.compareTo(s1)); 
+	} 
+} 
