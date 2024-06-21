a7beb7ed5d8a65e355ca95a7a4a03c2b7fc88ab0
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2024-06-21 23:54:00 +0530

    Update EnumStrings.java

--- EnumStrings.java
+++ EnumStrings.java
@@ -5,16 +5,47 @@
 *******************************************************************************/
 
 enum Student {
-   Nithin, Aslam, Sujan, Subash
+    Nithin, Aslam, Sujan, Subash
 }
 
-class Main {
-   public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
+        // Accessing enum constants
+        System.out.println("Enum constant Nithin: " + Student.Nithin);
+        System.out.println("Enum constant Aslam: " + Student.Aslam);
+        System.out.println("Enum constant Sujan: " + Student.Sujan);
+        System.out.println("Enum constant Subash: " + Student.Subash);
 
-    System.out.println("string value of SMALL is " + Student.Nithin.toString());
-    System.out.println("string value of MEDIUM is " + Student.Subash.name());
+        // Converting enum constants to string
+        System.out.println("String value of Nithin: " + Student.Nithin.toString());
+        System.out.println("String value of Subash: " + Student.Subash.name());
 
-   }
+        // Iterating through all enum constants
+        System.out.println("All Students:");
+        for (Student student : Student.values()) {
+            System.out.println(student);
+        }
+
+        // Switch-case with enums
+        Student selectedStudent = Student.Nithin;
+        switch (selectedStudent) {
+            case Nithin:
+                System.out.println("Nithin selected");
+                break;
+            case Aslam:
+                System.out.println("Aslam selected");
+                break;
+            case Sujan:
+                System.out.println("Sujan selected");
+                break;
+            case Subash:
+                System.out.println("Subash selected");
+                break;
+            default:
+                System.out.println("Unknown student selected");
+        }
+    }
 }
+
 
 
