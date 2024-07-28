7051beebd0b8316218d60a56c14eaf5af1be14e4
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2024-07-28 23:57:28 +0530

    Need to Fix Lambda.java

--- Lambda.java
+++ Lambda.java
@@ -11,6 +11,7 @@ import java.util.*;
 
 interface lambda{
     public void print();
+    public void print(int c,int d);
 }
 
 public class Main
@@ -18,9 +19,13 @@ public class Main
 	public static void main(String[] args) {
 	Scanner x=new Scanner(System.in);
 	String a=x.next(),b=x.next();
-	lambda s1=()->{
+	lambda s1=(c,d)->{
+	    System.out.print("It is "+c+" "+d);
+	};
+	lambda s2=()->{
 	    System.out.print("It is "+a+" "+b);
 	};
 	s1.print();
+	s1.print(5,8);
 	}
 }
